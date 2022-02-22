571e9fb8855db1dda286cf9842e6db1dcfadd49a
BenVos <ben.j.vos@gmail.com>
2022-02-22 11:13:33 +0100

    Added test functions and debug file

--- business/calc_test.go
+++ business/calc_test.go
@@ -1,40 +1,50 @@
 package business
 
 import (
+	"che/dt/week2/orders/types"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCalculateTotalOrderPrice_MultipleOrderLines_GivesExpectedTotal(t *testing.T) {
 
 	// Given
 
-	// order := types.Order{
-	// 	Id:          0,
-	// 	Organisatie: "Centuria",
-	// 	Regels: []types.OrderRegel{
-	// 		{
-	// 			Nummer:       0,
-	// 			Btw:          "21%",
-	// 			Aantal:       4,
-	// 			Prijs:        94.85,
-	// 			Omschrijving: "aute aute adipisicing",
-	// 		},
-	// 		{
-	// 			Nummer:       1,
-	// 			Btw:          "9%",
-	// 			Aantal:       2,
-	// 			Prijs:        163.07,
-	// 			Omschrijving: "aute aute adipisicing",
-	// 		},
-	// 	},
-	// 	Korting:    0,
-	// 	Definitief: true,
-	// 	Datum:      time.Now(),
-	// }
+	order := types.Order{
+		Id:          0,
+		Organisatie: "Centuria",
+		Regels: []types.OrderRegel{
+			{
+				Nummer:       0,
+				Btw:          "21%",
+				Aantal:       4,
+				Prijs:        93.85,
+				Omschrijving: "aute aute adipisicing",
+			},
+			{
+				Nummer:       1,
+				Btw:          "9%",
+				Aantal:       2,
+				Prijs:        163.07,
+				Omschrijving: "aute aute adipisicing",
+			},
+		},
+		Korting:    0,
+		Definitief: true,
+		Datum:      time.Now(),
+	}
 
 	// When
 
+	total := CalculateTotalOrderPrice(order)
+
 	// Then
+
+	assert.Equal(t, float32(809.7266), total)
+
+	// 809,73
 
 }
 
@@ -42,9 +52,22 @@ func TestCalculateTotalOrderPrice_NoOrderLines_GivesTotalZero(t *testing.T) {
 
 	// Given
 
+	order := types.Order{
+		Id:          0,
+		Organisatie: "Centuria",
+		Regels:      []types.OrderRegel{},
+		Korting:     0,
+		Definitief:  true,
+		Datum:       time.Now(),
+	}
+
 	// When
 
+	total := CalculateTotalOrderPrice(order)
+
 	// Then
+
+	assert.Equal(t, float32(0), total)
 
 }
 
@@ -52,9 +75,30 @@ func TestCalculateTotalOrderPrice_SingleOrderLine_GivesExpectedTotal(t *testing.
 
 	// Given
 
+	order := types.Order{
+		Id:          0,
+		Organisatie: "Centuria",
+		Regels: []types.OrderRegel{
+			{
+				Nummer:       0,
+				Btw:          "0%",
+				Aantal:       6,
+				Prijs:        27.35,
+				Omschrijving: "aute aute adipisicing",
+			},
+		},
+		Korting:    0,
+		Definitief: true,
+		Datum:      time.Now(),
+	}
+
 	// When
 
+	total := CalculateTotalOrderPrice(order)
+
 	// Then
+
+	assert.Equal(t, float32(164.10), total)
 
 }
 
