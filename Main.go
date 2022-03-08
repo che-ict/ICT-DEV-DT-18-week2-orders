8bd96222ba96d20d89b07a86b9b89c4fea6aa12a
basrijs1999 <bas.v.rijsewijk@gmail.com>
2022-03-08 11:06:32 +0100

    iets leuks toegevoegd

--- Main.go
+++ Main.go
@@ -15,6 +15,10 @@ func main() {
 
 	var orders []types.Order
 	business.ReadOrders(os.Args[1], &orders)
+	
+	foreach order in orders
+  	print(" - order.Organisatie: len(order.Regels), € CalculateTotalOrderPrice(order)")
+	
 
 	fmt.Printf("Aantal orders: %v\n", len(orders))
 	fmt.Printf("Total order price: %.2f\n", business.CalculateTotal(orders))
